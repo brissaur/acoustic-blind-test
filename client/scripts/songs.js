24ebce1c6995b8af0f756f51543e2186878f9066
brissaur <brissaudr@gmail.com>
2018-12-18 21:52:13 +0100

    feat(songs): initializing songs array for populate

--- client/scripts/songs.js
+++ client/scripts/songs.js
@@ -1,1 +1,207 @@
-module.exports = [{}];
+module.exports = [
+  {
+    title: "Inspecteur gadget",
+    artist: "",
+    comment: ""
+  }
+  //   {
+  //     title: "Your song",
+  //     artist: "Elton John",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Leave without all the rest",
+  //     artist: "Linking Park",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "A la pêche aux moules",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Chan Chan",
+  //     artist: "Cumpai Segundo",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Manhattan Kaboul",
+  //     artist: "Renaud",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Butterfly and hurricanes",
+  //     artist: "Muse",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "S'il sufisait d'aimer",
+  //     artist: "Celine Dion",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Le lion est mort ce soir",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Yesterdays",
+  //     artist: "The Beatles",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "La tribu de dana",
+  //     artist: "MANO",
+  //     comment: "Am"
+  //   },
+  //   {
+  //     title: "Spiderman",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Razmocket",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Wonderwall",
+  //     artist: "Oasis",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "No woman no cry",
+  //     artist: "Bob Marley",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Face à la mer",
+  //     artist: "Passi/Calogero",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "If I ain't got you",
+  //     artist: "Alicia Keys",
+  //     comment: "Em B7"
+  //   },
+  //   {
+  //     title: "Le jazz et la java",
+  //     artist: "Claude Nougaro",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Je me suis fait tout petit",
+  //     artist: "Brassens",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Voie sur ton chemin (CHORISTES)",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "42",
+  //     artist: "Coldplay",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Intouchable",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Champs Elyses",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Sarabande",
+  //     artist: "Haeendel",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Schtrumpf",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Ce matin un lapin",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Red red wine",
+  //     artist: "UB 40",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Sway",
+  //     artist: "Diams~",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Sunny",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Hit the road jack",
+  //     artist: "?",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Jimmy",
+  //     artist: "Moriarty",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Carioca",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Les feu de l'amour",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "I will survive",
+  //     artist: "",
+  //     comment: "la la la la la, la la la la la la,"
+  //   },
+  //   {
+  //     title: "Nain bilbo",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Quand on est con",
+  //     artist: "Brassens",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Aladin",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Pokemon cycling theme",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Asterix au cinema",
+  //     artist: "",
+  //     comment: ""
+  //   },
+  //   {
+  //     title: "Asterix pudding arsenic",
+  //     artist: "",
+  //     comment: ""
+  //   }
+];
